12d405df9c87e3c98e1a48423341bdc3c8317093
Armanskij <101628saMsunG>
2024-03-01 06:32:30 +0000

    alteracao de idioma

--- gasolina.py
+++ gasolina.py
@@ -1,11 +1,10 @@
 
-      import pandas as pd
-      import seaborn as sns
-      import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-      df = pd.read_csv('gasolina.csv', delimiter=',',encoding='utf8')
-
-      data = sns.lineplot(x='dia', y='venda', data=df)
-      data.set(title='Preço do Litro da Gasolina', xlabel='Dias', ylabel='Preço (R$)')
-      plt.savefig('gasolina.png')
+df = pd.read_csv('gasolina.csv', delimiter=',', encoding='utf8')
+data = sns.lineplot(x='dia', y='venda', data=df, color='red')
+data.set(title='Price of a Liter of Gasoline', xlabel='Days', ylabel='Price')
+plt.savefig('gasolina.png')
   
